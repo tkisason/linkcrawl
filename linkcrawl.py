1400572dcaccb4f20933bacf2c895a20ffd314e6
Tonimir Kisasondi <kisasondi@gmail.com>
2011-03-19 23:09:40 +0100

    Simple fixes and clean/all support

--- linkcrawl.py
+++ linkcrawl.py
@@ -15,22 +15,29 @@ class URLLister(SGMLParser):        # Simple SGML Parser for html href extractio
         if href:
             self.urls.extend(href)
 
-def GetLinks(site):
+def GetLinks(site,clean):
         parser = URLLister()
         parser.feed(urllib2.urlopen(site).read())
         parser.close()
         for url in parser.urls:
-            if url[:4] == ("http" or "https" or "ftp"):
+            if url.find("://") != -1:
                 Collectedlinks.append(str(url))
-            else:
-                Collectedlinks.append(str(sys.argv[1]+url))
+            elif url[0] == "/":
+                Collectedlinks.append(str(site+url))
+            elif clean == "-c":
+                print "w00p w00p"
+                Collectedlinks.append(url)
         parser.close()
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print "usage: ./"+sys.argv[0]+" http://www.example.com\n"
+    if len(sys.argv) == 1:
+        print "usage: ./"+sys.argv[0]+" [-c] http://www.example.com"
+        print "     -c: print only links excluding javascripts and mailtos...\n"
     else:
-        GetLinks(str(sys.argv[1]))
+        if sys.argv[1] == "-c":
+            GetLinks(str(sys.argv[2]),str(sys.argv[1]))
+        else:
+            GetLinks(str(sys.argv[1]),"")
         for link in Collectedlinks:
             print link
 
