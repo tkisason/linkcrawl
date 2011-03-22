083bca97bb0c02cff4fc628e80b3fdb9822ca0eb
Igor Vuk <parcijala@gmail.com>
2011-03-22 13:05:01 +0100

    Renamed the Worker function, it seems that upstream has that name reserved for Most Important Other ThingTM. :)

--- linkcrawl.py
+++ linkcrawl.py
@@ -25,37 +25,43 @@ def GetLinks(site,clean):
             if len(url) <= 1:
                 pass
             elif url.find("://") != -1:
-				if clean == "-o":
-					site1 = urlparse(site)
-					url1 = urlparse(url)
-					if site1.netloc == url1.netloc:
+                if clean == "-o":
+                    site1 = urlparse(site)
+                    url1 = urlparse(url)
+                    if site1.netloc == url1.netloc:
 						Collectedlinks.append(str(url))
-				else:
-					Collectedlinks.append(str(url))
+                    else:
+					    Collectedlinks.append(str(url))
             elif url[0] == "/":
                 Collectedlinks.append(str(site+url))
             elif clean == "-e":
                 Collectedlinks.append(url)
+            elif clean == "-r":
+                for url in Collectedlinks:
+                    GetLinks(url,"-r")
         parser.close()
 
-def Worker(param):
+def h_pref(param):
     if param.find('http://') != -1:
         return str(param)
     else:
         return str('http://'+param)
 
 if __name__ == '__main__':
-	if len(sys.argv) == 1:
-		print "usage: ./"+sys.argv[0]+" [OPTION] http://www.example.com"
-		print "     -e: print everything: links including javascripts and mailto attributes...\n"
-		print "     -o: print only those that have the same origin as the requested site\n"
-	else:
-		if sys.argv[1] == "-e":
-			GetLinks(Worker(str(sys.argv[2])),"-e")
-		elif sys.argv[1] == "-o":
-			GetLinks(Worker(str(sys.argv[2])),"-o")
-		else:
-			GetLinks(Worker(str(sys.argv[1])),"")
-		for link in Collectedlinks:
-			print link
+    if len(sys.argv) == 1:
+        print "usage: ./"+sys.argv[0]+" [OPTION] http://www.example.com"
+        print "     -e: print everything: links including javascripts and mailto attributes...\n"
+        print "     -o: print only those that have the same origin as the requested site\n"
+    else:
+        if sys.argv[1] == "-e":
+            GetLinks(h_pref(str(sys.argv[2])),"-e")
+        elif sys.argv[1] == "-o":
+            GetLinks(h_pref(str(sys.argv[2])),"-o")
+        elif sys.argv[1] == "-r":
+            GetLinks(h_pref(str(sys.argv[2])),"")
+            GetLinks(h_pref(str(sys.argv[2])),"-r")
+        else:
+			GetLinks(h_pref(str(sys.argv[1])),"")
+        for link in Collectedlinks:
+            print link
 
