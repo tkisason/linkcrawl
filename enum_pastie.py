3832f4172cbc6b787369b0af12cef66d47ca504d
Tonimir Kisasondi <kisasondi@gmail.com>
2011-04-17 14:00:51 +0200

    Improved enum_pastie.py, added a simple shell

--- enum_pastie.py
+++ enum_pastie.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python
+
 import linkcrawl
+import urllib2
+import sys
 
 def pastie_gen_url(query,page=1):
     links = []
@@ -9,7 +13,7 @@ def pastie_gen_url(query,page=1):
     return "http://pastie.org/search?commit=Start+Search&page="+str(page)+"&q="+query.replace(" ","+")
 
 
-def pastie_find(query):
+def pastie_find(query,raw="y"):
     urls = linkcrawl.same_netloc("http://pastie.org",linkcrawl.parser(pastie_gen_url(query)))
     ids = linkcrawl.extract_url_arg(urls,"page")
     big = int(ids[0])
@@ -24,5 +28,27 @@ def pastie_find(query):
     for link in links:
         if link.find("pastes/") != -1:
             if link.find("/new") == -1:
-                olinks.append(link+"/text")
+                if raw == "y":
+                    olinks.append(link+"/text")
+                else:
+                    olinks.append(link)
     return olinks
+
+if __name__ == '__main__':
+    q = raw_input("[+] LinkCrawl: Enumerate pastie.org, enter query: ")
+    OF = raw_input("[+] Enter output file name: ")
+    delimiter = "EOF EOF EOF"
+    links = pastie_find(q)
+    print "[+] Got links, dumping content to file, delimiter is: " + str(delimiter)
+    OF = OF+".html"
+    FILE = open((OF),"w")
+    sys.stdout.write("[+] Working: ")
+    for link in links:
+        FILE.write(urllib2.urlopen(link).read())
+        FILE.write("\n\n\n"+delimiter+"\n\n\n")
+        sys.stdout.write(".")
+        sys.stdout.flush()
+    print "\n"
+    FILE.close()
+    print "[+] Enumeration done, output is in: " + OF
+        
